75996ca888be2be22dea6e48d1b30ac873ddbce5
pulidovaldezh@gmail.com <pulidovaldezh@gmail.com>
2019-03-11 01:12:35 -0600

    Informacion general y pantalla de carga de usuario

---
+++ ConsultorioMMPI/frmDatosUsuario.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace ConsultorioMMPI
+{
+    partial class frmDatosUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblNacimiento = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblEscolaridad = new System.Windows.Forms.Label();
+            this.lblRegistro = new System.Windows.Forms.Label();
+            this.NumPrev = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblCivil = new System.Windows.Forms.Label();
+            this.lblOcupacion = new System.Windows.Forms.Label();
+            this.lblRecidencia = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtFechaNacimiento = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtEscolaridad = new System.Windows.Forms.TextBox();
+            this.txtFechaRegistro = new System.Windows.Forms.TextBox();
+            this.txtPeventivo = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtEstadoCivil = new System.Windows.Forms.TextBox();
+            this.txtOcupacion = new System.Windows.Forms.TextBox();
+            this.txtResidencia = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos del usuario";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(24, 51);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(68, 18);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblNacimiento
+            // 
+            this.lblNacimiento.AutoSize = true;
+            this.lblNacimiento.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNacimiento.Location = new System.Drawing.Point(24, 81);
+            this.lblNacimiento.Name = "lblNacimiento";
+            this.lblNacimiento.Size = new System.Drawing.Size(158, 18);
+            this.lblNacimiento.TabIndex = 2;
+            this.lblNacimiento.Text = "Fecha de nacimiento:";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSexo.Location = new System.Drawing.Point(24, 109);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(48, 18);
+            this.lblSexo.TabIndex = 3;
+            this.lblSexo.Text = "Sexo:";
+            // 
+            // lblEscolaridad
+            // 
+            this.lblEscolaridad.AutoSize = true;
+            this.lblEscolaridad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscolaridad.Location = new System.Drawing.Point(24, 138);
+            this.lblEscolaridad.Name = "lblEscolaridad";
+            this.lblEscolaridad.Size = new System.Drawing.Size(96, 18);
+            this.lblEscolaridad.TabIndex = 4;
+            this.lblEscolaridad.Text = "Escolaridad:";
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistro.Location = new System.Drawing.Point(24, 167);
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(135, 18);
+            this.lblRegistro.TabIndex = 5;
+            this.lblRegistro.Text = "Fecha de registro:";
+            // 
+            // NumPrev
+            // 
+            this.NumPrev.AutoSize = true;
+            this.NumPrev.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumPrev.Location = new System.Drawing.Point(391, 51);
+            this.NumPrev.Name = "NumPrev";
+            this.NumPrev.Size = new System.Drawing.Size(162, 18);
+            this.NumPrev.TabIndex = 6;
+            this.NumPrev.Text = "N° Medico Preventivo:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(391, 80);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(50, 18);
+            this.lblEdad.TabIndex = 7;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lblCivil
+            // 
+            this.lblCivil.AutoSize = true;
+            this.lblCivil.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCivil.Location = new System.Drawing.Point(391, 109);
+            this.lblCivil.Name = "lblCivil";
+            this.lblCivil.Size = new System.Drawing.Size(92, 18);
+            this.lblCivil.TabIndex = 8;
+            this.lblCivil.Text = "Estado civil:";
+            // 
+            // lblOcupacion
+            // 
+            this.lblOcupacion.AutoSize = true;
+            this.lblOcupacion.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOcupacion.Location = new System.Drawing.Point(391, 138);
+            this.lblOcupacion.Name = "lblOcupacion";
+            this.lblOcupacion.Size = new System.Drawing.Size(87, 18);
+            this.lblOcupacion.TabIndex = 9;
+            this.lblOcupacion.Text = "Ocupacion:";
+            // 
+            // lblRecidencia
+            // 
+            this.lblRecidencia.AutoSize = true;
+            this.lblRecidencia.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecidencia.Location = new System.Drawing.Point(391, 167);
+            this.lblRecidencia.Name = "lblRecidencia";
+            this.lblRecidencia.Size = new System.Drawing.Size(151, 18);
+            this.lblRecidencia.TabIndex = 10;
+            this.lblRecidencia.Text = "Lugar de residencia:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(391, 18);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(70, 18);
+            this.lblTelefono.TabIndex = 11;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(187, 52);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(179, 20);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // txtFechaNacimiento
+            // 
+            this.txtFechaNacimiento.Location = new System.Drawing.Point(187, 81);
+            this.txtFechaNacimiento.Name = "txtFechaNacimiento";
+            this.txtFechaNacimiento.Size = new System.Drawing.Size(179, 20);
+            this.txtFechaNacimiento.TabIndex = 13;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(187, 110);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(179, 20);
+            this.txtSexo.TabIndex = 14;
+            // 
+            // txtEscolaridad
+            // 
+            this.txtEscolaridad.Location = new System.Drawing.Point(187, 139);
+            this.txtEscolaridad.Name = "txtEscolaridad";
+            this.txtEscolaridad.Size = new System.Drawing.Size(179, 20);
+            this.txtEscolaridad.TabIndex = 15;
+            // 
+            // txtFechaRegistro
+            // 
+            this.txtFechaRegistro.Location = new System.Drawing.Point(187, 168);
+            this.txtFechaRegistro.Name = "txtFechaRegistro";
+            this.txtFechaRegistro.Size = new System.Drawing.Size(179, 20);
+            this.txtFechaRegistro.TabIndex = 16;
+            // 
+            // txtPeventivo
+            // 
+            this.txtPeventivo.Location = new System.Drawing.Point(554, 52);
+            this.txtPeventivo.Name = "txtPeventivo";
+            this.txtPeventivo.Size = new System.Drawing.Size(179, 20);
+            this.txtPeventivo.TabIndex = 17;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(554, 81);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(179, 20);
+            this.txtEdad.TabIndex = 18;
+            // 
+            // txtEstadoCivil
+            // 
+            this.txtEstadoCivil.Location = new System.Drawing.Point(554, 110);
+            this.txtEstadoCivil.Name = "txtEstadoCivil";
+            this.txtEstadoCivil.Size = new System.Drawing.Size(179, 20);
+            this.txtEstadoCivil.TabIndex = 19;
+            // 
+            // txtOcupacion
+            // 
+            this.txtOcupacion.Location = new System.Drawing.Point(554, 139);
+            this.txtOcupacion.Name = "txtOcupacion";
+            this.txtOcupacion.Size = new System.Drawing.Size(179, 20);
+            this.txtOcupacion.TabIndex = 20;
+            // 
+            // txtResidencia
+            // 
+            this.txtResidencia.Location = new System.Drawing.Point(554, 168);
+            this.txtResidencia.Name = "txtResidencia";
+            this.txtResidencia.Size = new System.Drawing.Size(179, 20);
+            this.txtResidencia.TabIndex = 21;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(554, 19);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(179, 20);
+            this.txtTelefono.TabIndex = 22;
+            // 
+            // frmDatosUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 214);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtResidencia);
+            this.Controls.Add(this.txtOcupacion);
+            this.Controls.Add(this.txtEstadoCivil);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtPeventivo);
+            this.Controls.Add(this.txtFechaRegistro);
+            this.Controls.Add(this.txtEscolaridad);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtFechaNacimiento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblRecidencia);
+            this.Controls.Add(this.lblOcupacion);
+            this.Controls.Add(this.lblCivil);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.NumPrev);
+            this.Controls.Add(this.lblRegistro);
+            this.Controls.Add(this.lblEscolaridad);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblNacimiento);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDatosUsuario";
+            this.Text = "frmDatosUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblNacimiento;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblEscolaridad;
+        private System.Windows.Forms.Label lblRegistro;
+        private System.Windows.Forms.Label NumPrev;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblCivil;
+        private System.Windows.Forms.Label lblOcupacion;
+        private System.Windows.Forms.Label lblRecidencia;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtFechaNacimiento;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.TextBox txtEscolaridad;
+        private System.Windows.Forms.TextBox txtFechaRegistro;
+        private System.Windows.Forms.TextBox txtPeventivo;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtEstadoCivil;
+        private System.Windows.Forms.TextBox txtOcupacion;
+        private System.Windows.Forms.TextBox txtResidencia;
+        private System.Windows.Forms.TextBox txtTelefono;
+    }
+}
--- ConsultorioMMPI/frmPrincipal.Designer.cs
+++ ConsultorioMMPI/frmPrincipal.Designer.cs
@@ -29,25 +29,72 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picboxLogo = new System.Windows.Forms.PictureBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(191, 94);
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(385, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Secretaria de comunicaciones y transportes terceros particulares";
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dr. De tal ";
+            // 
+            // picboxLogo
+            // 
+            this.picboxLogo.Location = new System.Drawing.Point(171, 168);
+            this.picboxLogo.Name = "picboxLogo";
+            this.picboxLogo.Size = new System.Drawing.Size(100, 50);
+            this.picboxLogo.TabIndex = 2;
+            this.picboxLogo.TabStop = false;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(27, 296);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 3;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(286, 283);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(470, 358);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.picboxLogo);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Información general";
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +103,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picboxLogo;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
 
