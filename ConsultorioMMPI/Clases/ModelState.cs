bbbda6c0193ef17bbeeddc8e9912645c21046292
pulidovaldezh@gmail.com <pulidovaldezh@gmail.com>
2019-03-11 23:05:45 -0600

    Puse una clase para validar campos mas izi

---
+++ ConsultorioMMPI/Clases/ModelState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsultorioMMPI.Clases
+{
+    public static class ModelState
+    {
+        public static List<string> ErrorMessages = new List<string>();
+
+        public static bool IsValid<T>(T model)
+        {
+            var validationContext = new ValidationContext(model, null, null);
+            var results = new List<ValidationResult>();
+
+            if (Validator.TryValidateObject(model, validationContext, results, true))
+            {
+                return true;
+            }
+            else
+            {
+                ErrorMessages = results.Select(x => x.ErrorMessage).ToList();
+                return false;
+            }
+        }
+    }
+}
